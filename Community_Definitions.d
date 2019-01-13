041fc32ea8431b7b9be53a7956299c1c483eed96
LaintheDegen <46644956+LaintheDegen@users.noreply.github.com>
2019-01-13 04:34:51 -0600

    Create Community_Definitions.d

---
+++ Community_Definitions.d
@@ -0,0 +1,5 @@
+# Community Definitions
+### Words as they are defined by their members
+
+Butt Touchers: When ever you stop worrying about what's right and wants wrong and you defend your buddy. -Defined by JonJon
+> Sethless butt touches Stargatemunky just to try and crush JonJon but they fail. He just keeps talking.
